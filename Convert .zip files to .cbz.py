5c4e1fd2d02c7734fa2240fec0c2aed570fc640e
yourally2 <yourally2@users.noreply.github.com>
2019-08-31 09:03:10 +0800

    fixed a couple of bugs

--- Convert .zip files to .cbz.py
+++ Convert .zip files to .cbz.py
@@ -3,17 +3,17 @@ from os.path import join
 from shutil import copyfile
 
 def convertzip(foldername):   
-    if foldername.endswith('.zip'):
+    if foldername.endswith('.zip') or foldername.endswith('.rar'):
         try:
             copyfile(join(root_path, foldername), join(src_path, foldername))
-            os.rename(foldername, foldername[:-4] + '.cbz' )
+            os.rename(join(src_path,foldername), join(src_path,foldername[:-4]) + '.cbz' )
             print('Zipping '+ foldername[:-4])
         except FileExistsError:
             print('File already exists, going to the next one')
         
 
 root_path = r'ROOT_DIRECTORY' #Directory with files to be zipped to .cbz
-src_path = os.getcwd()
+src_path = os.getcwd() #Directory to dump the .cbz files to
 
 for root, dirs, files in os.walk(root_path):  
     for file in files:
